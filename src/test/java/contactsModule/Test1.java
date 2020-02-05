c1cd52d117076d039265b5356d858739a9730227
Mikail Durur <mikail.drr@gmail.com>
2020-02-04 21:35:36 -0600

    first push

---
+++ src/test/java/contactsModule/Test1.java
@@ -0,0 +1,12 @@
+package contactsModule;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+    @Test
+
+    public void test(){
+        System.out.println("Hello from other side");
+    }
+}
